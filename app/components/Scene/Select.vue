141cf7b119bda7f6deb1702f30553415cd54f37a
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-09-01 15:30:51 +0700

    Update Select.vue

--- app/components/Scene/Select.vue
+++ app/components/Scene/Select.vue
@@ -1,7 +1,7 @@
 <template>
     <TresGroup>
         <TresGroup :position="[0, 0, 0]">
-            <TresGroup :position="[2, 0, 0]">
+            <TresGroup :position="[0, 0, 0]">
                 <primitive :object="FramePortugal.scene"/>
                 <primitive :object="FramePortugal.scene"/>
             </TresGroup>
@@ -10,12 +10,12 @@
 </template>
 
 <script setup>
-import { useGLTF, useAnimations, Html } from '@tresjs/cientos'
+import { useGLTF } from '@tresjs/cientos'
 
 const router = useRouter()
 
 const FrameBelanda = await useGLTF('/models/FrameBelanda.glb')
-FrameBelanda.scene.scale.set(0, 0, 0)
+FrameBelanda.scene.scale.set(0.5, 0.5, 0.5)
 const FrameSpanyol = await useGLTF('/models/FrameSpanyol.glb')
 const FramePortugal = await useGLTF('/models/Frame.glb')
 const FrameInggris = await useGLTF('/models/FrameInggris.glb')
