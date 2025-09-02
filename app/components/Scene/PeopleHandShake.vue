a7dd9d88edce3768532224511843d1730660c4e9
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-09-02 07:27:35 +0700

    penambahan 3D object untuk Perjanjian

---
+++ app/components/Scene/PeopleHandShake.vue
@@ -0,0 +1,13 @@
+<template>
+    <TresGroup>
+        <primitive :object="modelKapal.scene" />
+    </TresGroup>
+</template>
+
+<script setup>
+import { useTresContext } from '@tresjs/core'
+import { useGLTF } from '@tresjs/cientos'
+import * as THREE from 'three'
+const modelOrang = await useGLTF('/models/model.glb')
+modelKapal.scene.position.set(0, 1, 0)
+</script>
--- app/pages/perjanjian-tordesillas/index.vue
+++ app/pages/perjanjian-tordesillas/index.vue
@@ -1,64 +1,64 @@
 <template>
     <div class="relative min-h-screen bg-gray-100 flex items-center justify-center p-8">
-      <HomeButton/>
+        <HomeButton/>
         
-      <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-5xl">
+        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-5xl">
         <!-- Judul -->
-        <h1 class="text-2xl font-bold text-center mb-6">
-            Perjanjian Tordesillas (7 Juni 1494)
-        </h1>
+            <h1 class="text-2xl font-bold text-center mb-6">
+                Perjanjian Tordesillas (7 Juni 1494)
+            </h1>
 
-        <!-- Grid -->
-        <div class="grid grid-cols-2 gap-6">
-            <!-- Kolom kiri (scrollable) -->
-            <div class="h-96 overflow-y-auto space-y-4 pr-4">
-            <!-- Latar Belakang -->
-            <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-blue-400">
-                <h2 class="font-semibold mb-2 text-gray-800">Latar Belakang:</h2>
-                <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
-                <li>Spanyol & Portugis bersaing dalam pelayaran samudra.</li>
-                <li>Penemuan benua Amerika (1492) oleh Columbus menimbulkan sengketa wilayah.</li>
-                <li>Paus Alexander VI memediasi untuk mencegah konflik.</li>
-                </ul>
-            </div>
+            <!-- Grid -->
+            <div class="grid grid-cols-2 gap-6">
+                <!-- Kolom kiri (scrollable) -->
+                <div class="h-96 overflow-y-auto space-y-4 pr-4">
+                <!-- Latar Belakang -->
+                <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-blue-400">
+                    <h2 class="font-semibold mb-2 text-gray-800">Latar Belakang:</h2>
+                    <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
+                    <li>Spanyol & Portugis bersaing dalam pelayaran samudra.</li>
+                    <li>Penemuan benua Amerika (1492) oleh Columbus menimbulkan sengketa wilayah.</li>
+                    <li>Paus Alexander VI memediasi untuk mencegah konflik.</li>
+                    </ul>
+                </div>
 
-            <!-- Isi Perjanjian -->
-            <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-green-400">
-                <h2 class="font-semibold mb-2 text-gray-800">Isi Perjanjian:</h2>
-                <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
-                <li>Dunia dibagi dengan garis demarkasi vertikal di Samudra Atlantik (370 legua barat Kepulauan Tanjung Verde).</li>
-                <li>Timur garis → Portugis.</li>
-                <li>Barat garis → Spanyol.</li>
-                </ul>
-            </div>
+                <!-- Isi Perjanjian -->
+                <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-green-400">
+                    <h2 class="font-semibold mb-2 text-gray-800">Isi Perjanjian:</h2>
+                    <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
+                    <li>Dunia dibagi dengan garis demarkasi vertikal di Samudra Atlantik (370 legua barat Kepulauan Tanjung Verde).</li>
+                    <li>Timur garis → Portugis.</li>
+                    <li>Barat garis → Spanyol.</li>
+                    </ul>
+                </div>
 
-            <!-- Pembagian Wilayah -->
-            <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-yellow-400">
-                <h2 class="font-semibold mb-2 text-gray-800">Pembagian Wilayah:</h2>
-                <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
-                <li>Portugis: Afrika, India, Asia (termasuk Malaka & Maluku), Brasil (karena berada di timur garis).</li>
-                <li>Spanyol: Hampir seluruh Amerika (kecuali Brasil).</li>
-                </ul>
-            </div>
+                <!-- Pembagian Wilayah -->
+                <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-yellow-400">
+                    <h2 class="font-semibold mb-2 text-gray-800">Pembagian Wilayah:</h2>
+                    <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
+                    <li>Portugis: Afrika, India, Asia (termasuk Malaka & Maluku), Brasil (karena berada di timur garis).</li>
+                    <li>Spanyol: Hampir seluruh Amerika (kecuali Brasil).</li>
+                    </ul>
+                </div>
 
-            <!-- Dampak -->
-            <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-red-400">
-                <h2 class="font-semibold mb-2 text-gray-800">Dampak:</h2>
-                <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
-                <li>Portugis berkembang di Asia sebagai kekuatan maritim.</li>
-                <li>Spanyol menguasai Amerika & kemudian Filipina.</li>
-                <li>Menjadi awal kolonialisme global bangsa Eropa.</li>
-                </ul>
-            </div>
-            </div>
+                <!-- Dampak -->
+                <div class="bg-gray-50 p-4 rounded shadow-sm border-l-4 border-red-400">
+                    <h2 class="font-semibold mb-2 text-gray-800">Dampak:</h2>
+                    <ul class="list-disc list-inside text-sm space-y-1 text-gray-700">
+                    <li>Portugis berkembang di Asia sebagai kekuatan maritim.</li>
+                    <li>Spanyol menguasai Amerika & kemudian Filipina.</li>
+                    <li>Menjadi awal kolonialisme global bangsa Eropa.</li>
+                    </ul>
+                </div>
+                </div>
 
-            <!-- Kolom kanan (ilustrasi) -->
-            <div class="flex items-center justify-center">
-            <div class="w-64 h-80 bg-gray-300 rounded flex items-center justify-center shadow-inner">
-                <span class="text-gray-700 font-semibold">Ilustrasi</span>
+                <!-- Kolom kanan (ilustrasi) -->
+                <div class="flex items-center justify-center">
+                <div class="w-64 h-80 bg-gray-300 rounded flex items-center justify-center shadow-inner">
+                    <span class="text-gray-700 font-semibold">Ilustrasi</span>
+                </div>
+                </div>
             </div>
-            </div>
-        </div>
         </div>
     </div>
 </template>
